9a3345949aade2fc7520e1e15c8c9574f68c0dd2
Matthew Brett <matthew.brett@gmail.com>
2012-04-08 15:08:50 -0700

    RF+BF - refactor simplicial complexes, fix doctest
    Refactor simplicial complex routine, write tests, fix doctests.

--- formula/tests/test_utils.py
+++ formula/tests/test_utils.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 
-from .. utils import fullrank
+from .. utils import fullrank, simplicial_complex
 
 from numpy.testing import (assert_array_almost_equal,
                            assert_almost_equal,
@@ -24,3 +24,51 @@ def test_fullrank():
     assert_equal(Y3.shape, (40,4))
     assert_almost_equal(Y1, Y3)
 
+
+def assert_sequal(complexes, faces, facets):
+    # Test whether predicted result for simplicial_complex is correct
+    out_faces, out_facets, out_all = simplicial_complex(*complexes)
+    assert_equal(faces, out_faces)
+    assert_equal(set(facets), set(out_facets))
+    face_all = sum([list(f) for f in faces.values()], [])
+    assert_equal(set(face_all), set(out_all))
+
+
+def test_simplicial():
+    # Test simplicial complex generation
+    assert_sequal((), {0: set()}, [])
+    assert_sequal((('a',)),
+                 {0: set(), 1: set([('a',)])}, [('a',)])
+    assert_sequal((('a','b'),),
+                  {0: set(), 1: set([('a',), ('b',)]), 2: set([('a', 'b')])},
+                  [('a', 'b')])
+    assert_sequal((('a','b'), ('a',)),
+                  {0: set(), 1: set([('a',), ('b',)]), 2: set([('a', 'b')])},
+                  [('a', 'b')])
+    assert_sequal((('a','b'), ('a','b')),
+                  {0: set(), 1: set([('a',), ('b',)]), 2: set([('a', 'b')])},
+                  [('a', 'b')])
+    assert_sequal((('a','b'), ('a','c')),
+                 {0: set(),
+                  1: set([('a',), ('b',), ('c',)]),
+                  2: set([('a', 'b'), ('a', 'c')])},
+                  [('a', 'b'), ('a', 'c')])
+    assert_sequal((('a','b','c'), ('a','c')),
+                 {0: set(),
+                  1: set([('a',), ('b',), ('c',)]),
+                  2: set([('a', 'b'), ('b', 'c'), ('a', 'c')]),
+                  3: set([('a', 'b', 'c')])},
+                  [('a', 'b', 'c')])
+    assert_sequal((('a','b','c'), ('a','d')),
+                 {0: set(),
+                  1: set([('a',), ('b',), ('c',), ('d',)]),
+                  2: set([('a', 'b'), ('b', 'c'), ('a', 'c'), ('a', 'd')]),
+                  3: set([('a', 'b', 'c')])},
+                  [('a', 'b', 'c'), ('a', 'd')])
+    assert_sequal((('a','b','c'), ('c','d'), ('e','f'), ('e',)),
+                  {0: set([]),
+                   1: set([('a',), ('c',), ('b',), ('e',), ('d',), ('f',)]),
+                   2: set([('b', 'c'), ('a', 'b'), ('e', 'f'),
+                           ('c', 'd'), ('a', 'c')]),
+                   3: set([('a', 'b', 'c')])},
+                  [('c', 'd'), ('e', 'f'), ('a', 'b', 'c')])
